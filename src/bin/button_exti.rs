5376548b9d4d65dce0fe7d78c0ca236dd4c2dafc
Harry Chen <baffen227@gmail.com>
2024-10-24 17:33:43 +0800

    Add button_exti.rs

---
+++ src/bin/button_exti.rs
@@ -0,0 +1,32 @@
+#![no_std]
+#![no_main]
+
+use defmt::*;
+use defmt_rtt as _;
+use embassy_executor::Spawner;
+use embassy_stm32::{
+	exti::ExtiInput,
+	gpio::{Input, Pull},
+};
+use panic_probe as _;
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+	let p = embassy_stm32::init(Default::default());
+	info!("Hello World!");
+
+	let mut button = ExtiInput::new(Input::new(p.PC13, Pull::Down), p.EXTI13);
+
+	info!("Press the USER button...");
+
+	loop {
+		button
+			.wait_for_rising_edge()
+			.await;
+		info!("Pressed!");
+		button
+			.wait_for_falling_edge()
+			.await;
+		info!("Released!");
+	}
+}
